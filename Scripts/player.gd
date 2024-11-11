ef3a7eb5b2da74bc633b4f1730da39b2bcc04b4b
Thallison Rhanniel <thallisonrlima@gmail.com>
2024-11-11 19:02:59 -0300

    Added some types and add vs code laun.json

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED: float = 130.0
+const JUMP_VELOCITY: float = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting ("physics/2d/default_gravity")
@@ -28,7 +28,6 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
-	#Play Animation
 	
 	if is_on_floor():
 		if direction == 0:
